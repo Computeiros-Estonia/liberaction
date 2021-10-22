71c5b66f9850dc1c601850c2e789a3eb4aec061c
agah <henriaguiar99@gmail.com>
2021-10-21 22:43:21 -0300

    Removed rest framework

--- liberaction/settings.py
+++ liberaction/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'liberaction.users',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -105,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
--- liberaction/users/views.py
+++ liberaction/users/views.py
@@ -1,7 +1,1 @@
-from rest_framework import viewsets
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.exclude(is_superuser=True)
-    serializer_class = UserSerializer
