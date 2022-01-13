657263c379f0e377ce06b3954af2b700cb079092
Henrique Zanetti <henriaguiar99@gmail.com>
2022-01-13 15:38:31 -0300

    Melhorado formulário de produto

--- liberaction/core/forms.py
+++ liberaction/core/forms.py
@@ -1,9 +1,13 @@
+from faulthandler import disable
 from django import forms
 from .models import Product
+from django.contrib.auth.models import User
 
 class ProductForm(forms.ModelForm):
     images = forms.FileField(
         widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    owner = forms.ModelChoiceField(
+        queryset=User.objects.all(), disabled=True)
     class Meta:
         model = Product
         fields = '__all__'
