de6b7ac995043a9639f0b14a95e93942a1331edc
Henrique Zanetti <henriaguiar99@gmail.com>
2022-01-19 14:53:26 -0300

    Escondidos campos automaticos

--- liberaction/core/forms.py
+++ liberaction/core/forms.py
@@ -6,7 +6,9 @@ class BaseProductForm(forms.ModelForm):
     images = forms.FileField(
         widget=forms.ClearableFileInput(attrs={'multiple': True}), required=False)
     owner = forms.ModelChoiceField(
-        queryset=User.objects.all(), disabled=True)
+        queryset=User.objects.all(), disabled=True,
+        widget=forms.HiddenInput()
+    )
     tags = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(), required=False)
     class Meta:
@@ -16,7 +18,9 @@ class BaseProductForm(forms.ModelForm):
 class ProductForm(forms.ModelForm):
     base = forms.ModelChoiceField(
         queryset=BaseProduct.objects.all(),
-        required=False, disabled=True)
+        required=False, disabled=True,
+        widget=forms.HiddenInput()
+    )
     class Meta:
         model = Product
         fields = '__all__'
@@ -24,7 +28,9 @@ class ProductForm(forms.ModelForm):
 class ServiceForm(forms.ModelForm):
     base = forms.ModelChoiceField(
         queryset=BaseProduct.objects.all(),
-        required=False, disabled=True)
+        required=False, disabled=True,
+        widget=forms.HiddenInput()
+    )
     class Meta:
         model = Service
         fields = '__all__'
