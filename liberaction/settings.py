d92b4064a97919ed340af8e246decdf26c4bf5f4
Henrique Zanetti <henriaguiar99@gmail.com>
2022-01-19 16:31:33 -0300

    Concertados links de arquivos estaticos

--- liberaction/settings.py
+++ liberaction/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'liberaction/static/'
+STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'liberaction/static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'liberaction/static'),
 ]
