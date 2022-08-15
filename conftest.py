79ba06572177785e6caaf91852e0beba2b7cb9a2
Henrique Zanetti <henriaguiar99@gmail.com>
2022-08-15 20:16:29 -0300

    Changed user model

--- conftest.py
+++ conftest.py
@@ -4,4 +4,4 @@ from liberaction.users.models import User
 
 @pytest.fixture
 def user(db):
-    return User.objects.create(email='root@liberaction.com.br', password='toor', birth_date='1999-01-01')
+    return User.objects.create(email='root@liberaction.com.br', password='toor')
