04a9e8a0392038cb919da206b8588e7bb86c0b69
Henrique Zanetti <henriaguiar99@gmail.com>
2022-08-15 20:30:14 -0300

    Added service on admin page

--- liberaction/core/admin.py
+++ liberaction/core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Tag, BaseProduct, Product, Review, Album, Picture
+from .models import Service, Tag, BaseProduct, Product, Review, Album, Picture
 
 admin.site.register(Tag)
 admin.site.register(Product)
+admin.site.register(Service)
 admin.site.register(BaseProduct)
 admin.site.register(Review)
 admin.site.register(Album)
